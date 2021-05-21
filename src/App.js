d5b4b79edbf83dd902ce3d5a6946de1774fd7c46
Bruno <sumar.bruno@yahoo.com>
2021-05-20 21:06:29 -0300

    listas de tarefas

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import {useState} from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+function App() {
+    const [showAddTask, setShowAddTask] = useState(false)
+    const [tasks, setTasks] = useState([])
+
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task)=>task.id !== id))
+    }
+
+    const toggleReminder = (id) => {
+      setTasks(
+        tasks.map((task)=>(
+          task.id === id ?
+            {...task, reminder: !task.reminder}:
+            task
+        ))
+      )
+    }
+
+    const addTask = (task) => {
+      const id = Math.floor(Math.random()*10000)
+      setTasks([...tasks, {id, ...task}])
+    }
+
+    const toggleAddTask = () => {
+      setShowAddTask(!showAddTask)
+    }
+
+    return (
+      <div className="container">
+        <Header
+          onAdd={toggleAddTask}
+          showAdd={showAddTask}
+        />
+        {showAddTask && <AddTask onAdd={addTask}/>}
+        { (tasks.length > 0) ?
+          <Tasks
+            tasks={tasks}
+            onDelete={deleteTask}
+            onToggle={toggleReminder}
+          /> :
+          <p className='center'>Nenhuma tarefa salva.</p>
+        }
+      </div>
+    )
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title, onAdd, showAdd}) => {
+    return (
+        <div className='header'>
+          <h1>{title}</h1>
+          <Button
+            color='green'
+            text={showAdd? 'Fechar':'Adicionar'}
+            onClick={onAdd}
+          />
+        </div>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Lista de tarefas',
+}
+
+Header.propTypes = {
+    title: PropTypes.string,
+}
+
+export default Header
